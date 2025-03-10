eb431dc6d8190381a54a0cf9229f1244b4a0b174
20221936clint <20221936@nbsc.edu.ph>
2025-03-10 21:52:55 +0800

    Create an user interface for user registration and login and added some features Like Wrong Combination Email and Password (AlertBox), API Error (Toast), Login Success (Toast).This will create a mock logic for the user login.

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,78 +1,98 @@
 import { 
   IonAvatar,
-    IonButton,
-    IonButtons,
-      IonCol,
-      IonContent, 
-      IonGrid, 
-      IonHeader, 
-      IonInput, 
-      IonInputPasswordToggle, 
-      IonMenuButton, 
-      IonPage, 
-      IonRow, 
-      IonTitle, 
-      IonToolbar, 
-      useIonRouter
-  } from '@ionic/react';
-  
-  const Login: React.FC = () => {
-    const navigation = useIonRouter();
+  IonButton,
+  IonCol,
+  IonContent, 
+  IonGrid, 
+  IonHeader, 
+  IonInput, 
+  IonInputPasswordToggle, 
+  IonPage, 
+  IonRow, 
+  IonTitle, 
+  IonToolbar, 
+  useIonRouter,
+  useIonAlert,
+  useIonToast
+} from '@ionic/react';
+import { useState } from 'react';
 
-    const doLogin = () => {
-        navigation.push('/it35-lab/app','forward','replace');
+const Login: React.FC = () => {
+  const navigation = useIonRouter();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [presentAlert] = useIonAlert();
+  const [presentToast] = useIonToast();
+
+  const doLogin = () => {
+    if (email === '20221936@nbsc.edu.ph' && password === '20221936') {
+      presentToast({
+        message: 'Login Successful!',
+        duration: 2000,
+        color: 'success'
+      });
+      navigation.push('/it35-lab/app', 'forward', 'replace');
+    } else if (email === '' || password === '') {
+      presentAlert({
+        header: 'Login Failed',
+        message: 'Please enter email and password!',
+        buttons: ['OK']
+      });
+    } else {
+      presentToast({
+        message: 'Wrong combination of Email and Password!',
+        duration: 2000,
+        color: 'danger'
+      });
     }
-    return (
-      <IonPage>
-        <IonHeader>
-          <IonToolbar>
-            <IonTitle>Login</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent className='ion-padding'>
-        
-        <div
-          style={{
-            marginTop: '25%',
-          }}
-        >
-          <IonGrid style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            height: '100%',
-          }}>
-            <IonRow >
-              
-              <IonCol size="8">
-                <IonAvatar style={{
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                  }}>
-                  <img 
-                  style={{
-                    width:'100px',
-                    objectFit:'cover',
-                  }}
-                  alt="Silhouette of a person's head" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5HmQCbPMJc3NiRlv1q9JC-zDKJX4sdXjJbA&s" />
-                </IonAvatar>
+  };
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Login</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent className='ion-padding'>
+        <div style={{ textAlign: 'center', marginTop: '10%' }}>
+          <IonAvatar style={{ margin: '0 auto 10px', display: 'block', width: '120px', height: '120px', borderRadius: '50%', overflow: 'hidden' }}>
+            <img 
+              style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+              alt="Silhouette of a person's head" 
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5HmQCbPMJc3NiRlv1q9JC-zDKJX4sdXjJbA&s" 
+            />
+          </IonAvatar>
+          <IonGrid>
+            <IonRow>
+              <IonCol size="12" style={{ marginTop: '10px', textAlign: 'left' }}>
+                <IonInput 
+                  label="Email" 
+                  type="email" 
+                  value={email} 
+                  onIonInput={(e) => setEmail(e.detail.value!)}
+                  required
+                  style={{ textAlign: 'left' }}
+                />
+                <IonInput 
+                  label="Password" 
+                  type="password" 
+                  value={password} 
+                  onIonInput={(e) => setPassword(e.detail.value!)}
+                  required
+                  style={{ textAlign: 'left' }}
+                >
+                  <IonInputPasswordToggle slot="end" />
+                </IonInput>
+                <IonButton onClick={doLogin} expand="full">Login</IonButton>
               </IonCol>
             </IonRow>
           </IonGrid>
-          <IonInput label="Username" >
-          </IonInput>
-          <IonInput type="password" label="Password" value="truth-hurts">
-            <IonInputPasswordToggle  slot="end"></IonInputPasswordToggle>
-          </IonInput>
-          <IonButton onClick={() => doLogin()} expand="full">
-            Login
-          </IonButton>
+
         </div>
-            
-        </IonContent>
-      </IonPage>
-    );
-  };
-  
-  export default Login;
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Login;
